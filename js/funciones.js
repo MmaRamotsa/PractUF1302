a41e227866a4a57e5a32e5c81b9d983a9da29122
Marian Martínez <ma.martinez68@gmail.com>
2017-01-25 10:31:30 +0100

    Añadidos todos los ficheros de la práctica. MAMG

---
+++ js/funciones.js
@@ -0,0 +1,4 @@
+// Código JavaScript utilizado en la página
+document.write("<p>=================================================</p>");
+document.write("<p><b>|&nbsp; &nbsp; <mark>ESTE PÁRRAFO ESTÁ ESCRITO DESDE JAVASCRIP</mark> &nbsp; &nbsp;|</b></p>");
+document.write("<p>=================================================</p>");
